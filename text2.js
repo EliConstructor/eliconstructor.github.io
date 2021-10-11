cb1ac8261567f4dc48f885b2952bb5fcddddafb2
EliConstructor <32846069+EliConstructor@users.noreply.github.com>
2021-10-11 17:55:49 +0300

    Banana

---
+++ text2.js
@@ -0,0 +1,19 @@
+function reportWindowSize() {
+    var d = document.getElementById('text');
+    d.style.position = "absolute";
+    d.style.left = (window.innerWidth/2)-d.offsetWidth/2+'px';
+    d.style.top = window.innerHeight*0.75+'px';
+}
+
+function onLoad() {
+    reportWindowSize();
+    var d = document.getElementById('text');
+    d.onclick = redirect;
+    setInterval(reportWindowSize, 20);
+}
+
+function redirect(){
+    window.location.href = "/";
+}
+  
+window.onload = onLoad;
